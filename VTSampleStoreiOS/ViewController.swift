937da32e47f000094f1bd01a156918e69660b5d3
Muhammad Anis <muhammad.anis@veritrans.co.id>
2014-12-12 14:04:37 +0700

    Initial Commit

---
+++ VTSampleStoreiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VTSampleStoreiOS
+//
+//  Created by Muhammad Anis on 12/12/14.
+//  Copyright (c) 2014 Veritrans Indonesia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
