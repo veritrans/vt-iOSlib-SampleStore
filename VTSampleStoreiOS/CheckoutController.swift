842608a3cca9cd7779d2643b3e1e1a141a192f8d
Muhammad Anis <muhammad.anis@veritrans.co.id>
2014-12-30 17:23:42 +0700

    Adding web view to get token with 3d secure

--- VTSampleStoreiOS/CheckoutController.swift
+++ VTSampleStoreiOS/CheckoutController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class CheckoutController : UIViewController, UITableViewDataSource, UITableViewDelegate
+class CheckoutController : UIViewController, UITableViewDataSource, UITableViewDelegate, UIWebViewDelegate
 {
     
     @IBOutlet weak var totalPriceLabel: UILabel!
+    
+    @IBOutlet weak var tokenLabel: UILabel!
+    
+    var totalPrice : Int = 0
     
     var checkoutItems : [VTCheckout] = []
     
@@ -21,13 +25,64 @@ class CheckoutController : UIViewController, UITableViewDataSource, UITableViewD
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var totalPrice : Int = 0
         
         for(checkoutItem) in checkoutItems{
-            totalPrice += checkoutItem.item.price * checkoutItem.quantity
+            self.totalPrice += checkoutItem.item.price * checkoutItem.quantity
         }
-        totalPriceLabel.text = "Total Price: \(totalPrice)"
+        totalPriceLabel.text = "Total Price: \(self.totalPrice)"
         
+        
+        VTConfig.setCLIENT_KEY("VT-client-3YUXFj6X0XBpeDgf")
+        VTConfig.setVT_IsProduction(false)
+        
+    }
+    
+    @IBAction func payBtnClikc(sender: AnyObject) {
+        
+        var vtDirect = VTDirect()
+        var cardDetails = VTCardDetails()
+        
+        //TODO: Input your customer's credit card from input
+        cardDetails.card_number = "4811111111111114"
+        cardDetails.card_cvv = "123"
+        cardDetails.card_exp_month = 1
+        cardDetails.card_exp_year = 2020;
+        cardDetails.secure = true
+        cardDetails.gross_amount = "\(self.totalPrice)"
+        
+        vtDirect.card_details = cardDetails
+        
+        vtDirect.getToken { (token:VTToken!, ex : NSException!) -> Void in
+            if(ex == nil){
+                self.tokenLabel.text = "Token: \(token.token_id)"
+                if(token.redirect_url != nil){
+                    let webView:UIWebView = UIWebView(frame: CGRectMake(0, 0, self.view.bounds.width, self.view.bounds.height))
+                    webView.loadRequest(NSURLRequest(URL: NSURL(string:token.redirect_url)!))
+                    webView.delegate = self
+                    self.view.addSubview(webView)
+                    
+                }
+                
+            }else{
+                println(ex.reason)
+            }
+        }
+    }
+    
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        return true;
+    }
+    
+    func webView(webView: UIWebView, didFailLoadWithError error: NSError) {
+        println("WebView start with error \(error)")
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        if(webView.request?.URL.absoluteString?.rangeOfString("callback") != nil){
+            //remove webview from parent
+            //TODO: charge user and check whether transaction is success
+            webView.removeFromSuperview();
+        }
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
